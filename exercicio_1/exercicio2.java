7aae212411b044b03b09f2aeccac530e6d119fd3
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2023-10-22 13:14:43 -0300

    Add files via upload

---
+++ exercicio_1/exercicio2.java
@@ -0,0 +1,12 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+
+        int x;
+        double y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(String.format("%.1f", y));
+    }
+
+}
